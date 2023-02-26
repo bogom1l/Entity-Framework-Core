553a6d15f833afed5ea81c4aad39666307c05de2
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2023-02-26 18:23:12 +0200

    will replace later

---
+++ 02. Entity Framework Introduction - Exercise/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,120 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+
+            string result = GetEmployeesFullInformation(dbContext);
+            Console.WriteLine(result);
+        }
+
+        //Problem 03
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 05
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employeesRnD = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in employeesRnD)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 06
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            Address newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            Employee? employee = context.Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+
+            employee!.Address = newAddress; //employee!
+
+            context.SaveChanges();
+
+            var employeeAddresses = context.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Take(10)
+                .Select(e => e.Address!.AddressText)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, employeeAddresses);
+        }
+
+        //Problem 07 - the task is wrong
+
+        //Problem 14
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            IQueryable<EmployeeProject> epToDelete = context.EmployeesProjects
+                .Where(ep => ep.ProjectId == 2);
+            context.EmployeesProjects.RemoveRange(epToDelete);
+
+            Project projecToDelete = context.Projects.Find(2)!;
+            context.Projects.Remove(projecToDelete);
+            
+            context.SaveChanges();
+
+            string[] projectNames = context.Projects
+                .Take(10)
+                .Select(p => p.Name)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, projectNames);
+        }
+
+
+
+
+
+    }
+}
