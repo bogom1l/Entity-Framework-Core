b50c56a2130de02f6bcc82ffb4d8bbefae301b56
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2023-03-05 13:09:41 +0200

    linq exc done

---
+++ 04. LINQ - Exercise/1/05.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MusicHub.Data.Models.Enums;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        public Song()
+        {
+            this.SongPerformers = new HashSet<SongPerformer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(ValidationConstants.SongNameMaxLength)]
+        public string Name { get; set; } = null!; //this is Required
+
+        public TimeSpan Duration { get; set; } //Required by default
+
+        public DateTime CreatedOn { get; set; } //Required by default
+
+        public Genre Genre { get; set; } //Required by default
+
+        [ForeignKey(nameof(Album))]
+        public int? AlbumId { get; set; }
+        public virtual Album? Album { get; set; }
+
+        [ForeignKey(nameof(Writer))]
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public virtual ICollection<SongPerformer> SongPerformers { get; set; }
+
+    }
+}
