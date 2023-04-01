a721f825cc15bfa64869dfb8393802bcdaad0997
wavezM5 <102171820+wavezM5@users.noreply.github.com>
2023-04-01 16:34:10 +0300

    exam done excellent6

---
+++ 99. Actual Exam/01-Model-Definition-Skeleton/Boardgames/DataProcessor/ImportDto/ImportSellerDto.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImportSellerDto
+    {
+        [JsonProperty("Name")]
+        [Required]
+        [MinLength(5)]
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty("Address")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
+        public string Address { get; set; } = null!;
+
+        [JsonProperty("Country")]
+        [Required]
+        public string Country { get; set; } = null!;
+
+        [JsonProperty("Website")]
+        [Required] 
+        [RegularExpression(@"^(www.[A-z-\d]+.com)$")]
+        public string Website { get; set; } = null!;
+
+        [JsonProperty("Boardgames")]
+        public int[] BoardgameIds { get; set; }
+
+
+    }
+}
---
+++ 99. Actual Exam/01-Model-Definition-Skeleton/Boardgames/DataProcessor/Serializer.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Boardgames.DataProcessor
+{
+    using Boardgames.Data;
+    using Boardgames.DataProcessor.ExportDto;
+    using Boardgames.Utilities;
+    using Newtonsoft.Json;
+
+    public class Serializer
+    {  
+        private static XmlHelper xmlHelper;
+
+        public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
+        {
+            xmlHelper = new XmlHelper();
+
+            ExportCreatorDto[] creators = context
+               .Creators
+                .Where(c => c.Boardgames.Any())
+                .Select(c => new ExportCreatorDto()
+                {
+                    CreatorName = c.FirstName + " " + c.LastName,
+                    BoardgamesCount = c.Boardgames.Count,
+                    Boardgames = c.Boardgames
+                        .Select(b => new ExportBoardgameDto()
+                        {
+                            BoardgameName = b.Name, //
+                            BoardgameYearPublished = b.YearPublished //
+                        })
+                        .OrderBy(b => b.BoardgameName)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.BoardgamesCount)
+                .ThenBy(c => c.CreatorName)
+                .ToArray();
+
+            return xmlHelper.Serialize(creators, "Creators");
+        }
+
+        public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
+        {
+            var sellers = context.Sellers
+                .Include(s => s.BoardgamesSellers)
+                .ThenInclude(ct => ct.Boardgame)
+                .AsNoTracking()
+                .ToArray()
+                .Where(s => s.BoardgamesSellers.Any(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating))
+                .Select(s => new
+                {
+                    s.Name,
+                    s.Website,
+                    Boardgames = s.BoardgamesSellers
+                        .Where(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating)
+                        .Select(bs => new
+                        {
+                            Name = bs.Boardgame.Name,
+                            Rating = bs.Boardgame.Rating,
+                            Mechanics = bs.Boardgame.Mechanics,
+                            Category = bs.Boardgame.CategoryType.ToString()
+                        })
+                        .OrderByDescending(b => b.Rating)
+                        .ThenBy(b => b.Name)
+                        .ToArray()
+                })
+                .OrderByDescending(s => s.Boardgames.Length)
+                .ThenBy(s => s.Name)
+                .Take(5)
+                .ToArray()
+                .ToArray();
+
+            //TODO: if doesnt work, try DTO method and uncomment Includes
+
+            return JsonConvert.SerializeObject(sellers, Formatting.Indented);
+        }
+    }
+}
